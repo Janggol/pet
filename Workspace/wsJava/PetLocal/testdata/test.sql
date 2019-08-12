418da897b83cce38274496dc154afb2672e27d2f
Janggol <heebum1216@gmail.com>
2019-08-12 20:42:22 +0900

    JHB

---
+++ Workspace/wsJava/PetLocal/testdata/test.sql
@@ -0,0 +1,19 @@
+-- 회원정보
+INSERT INTO MBER_INFO (MBER_NO, MBER_SE, ID, PW, NM, NCNM, BRTHDY, ADRES, TEL, EMAIL, INTRST_ANIMAL, isDelete) VALUES ('1', '1', 'test', 'test', 'test', 'test', '20190812', '대구', '01012341234', 'test@test.com', 'test', 'n');
+
+
+-- 게시판공통
+
+
+-- 일반게시판
+
+
+-- 꽃길게시판
+
+
+-- 평가
+
+
+-- 광고정보
+
+
